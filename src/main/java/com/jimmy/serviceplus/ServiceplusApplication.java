56f7d3f7096124adbe1d21bd444b2c169b8b46e9
cakpo <cyr.loko@yahoo.com>
2021-04-03 12:39:17 +0200

    Initialisation

---
+++ src/main/java/com/jimmy/serviceplus/ServiceplusApplication.java
@@ -0,0 +1,13 @@
+package com.jimmy.serviceplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceplusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jimmy/serviceplus/ServiceplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimmy.serviceplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
